352bcd7f3aca7d9d58b10c7b31a2dc9a5c07e440
Rabeeb Aqdas Jilani <63879512+Rabeebaqdas@users.noreply.github.com>
2023-02-01 18:06:14 +0500

    Update hardhat.config.js

--- contract/hardhat.config.js
+++ contract/hardhat.config.js
@@ -6,8 +6,8 @@ module.exports = {
   solidity : '0.8.0', // solidity version of smart contract
   networks : {
     ropsten : {
-      url :"https://eth-ropsten.alchemyapi.io/v2/Mn1o4wfYENOBFm3nnicuRZrk9yaBK_xA", // alchemy url for the deployment of smart contract
-      accounts : ['5891b493c71e777c3d64d82695e379e4738600dfcbce1dc272c6c08e3f094c35'] //private key
+      url :"https://eth-ropsten.alchemyapi.io/v2/ALCHEMY_API_KEY", // alchemy url for the deployment of smart contract
+      accounts : [''] //private key
     }
   }
 }
